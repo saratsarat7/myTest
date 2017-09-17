93d658aee5aad4191a88e69c422985f8ac2aafd7
Sarat <31168235+saratsarat7@users.noreply.github.com>
2017-09-17 22:11:42 +0530

    Updating file name standarda

--- offset.c
+++ offset.c
@@ -6,16 +6,19 @@
 #include "accessdata.h"
 #include "vars.h"
 
-static char fileName[] = "ruleDB.dsg";
+static char statFilename[]	= "Static_File.dsg";
+static char inFilename[]	= "Input.dsg";
+static char ruleFilename[]	= "Rule.dsg";
+static char outFilename[]	= "Output.dsg";
 
 void main()
 {
 	// struct staticVar svar;
 	struct staticVar *p;
-	
+
 	p = malloc(sizeof(struct staticVar));
 	// p = &svar;
-	
+
 	// strcpy(svar.s1, "Sarat");
 	// svar.s2 = 10;
 	// svar.s3 = 16.5;
@@ -25,15 +28,15 @@ void main()
 	// svar.s7 = 143.23;
 	// svar.s8.varId = 10;
 	// strcpy(svar.s8.varData,"Inside a structure.");
-	
+
 	FILE * fp;
-	
-	// fp = fopen (fileName, "w+");
-	fp = fopen (fileName, "r+");
+
+	// fp = fopen (statFilename, "w+");
+	fp = fopen (statFilename, "r+");
 	// fwrite(p,sizeof(struct staticVar),1,fp);
 	fread(p,sizeof(struct staticVar),1,fp);
-	
-	printf("%s \n", p->s4);
-	
+
+	printf("%s \n", p->s8.varData);
+
 	fclose(fp);
 }
